3903db4cd6061bf1f2a74f7fff23b910ebcc260d
Tejesh <tejesh.allampati@zemosolabs.com>
2021-09-13 12:10:43 +0530

    public releaset

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -7,7 +7,7 @@ if [ -z $2 ]
 then
 	sed -E "s/versionName .+/versionName \"$SEMVER\"/g" app/build.gradle > tempfile && cat tempfile > app/build.gradle && rm -f tempfile
 else
-	sed -E "s/versionName .+/versionName \"$SEMVER-$2\"/g" app/build.gradle > tempfile && cat tempfile > app/build.gradle && rm -f tempfile
+	sed -E "s/versionName .+/versionName \"$SEMVER-dev$2\"/g" app/build.gradle > tempfile && cat tempfile > app/build.gradle && rm -f tempfile
 fi
 
 echo --------------------------
