9451786e06c439b394b07255f43d365f80244127
Tejesh <tejesh.allampati@zemosolabs.com>
2021-09-13 10:41:36 +0530

    added Internal release workflow

---
+++ scripts/bump_version.sh
@@ -0,0 +1,14 @@
+echo "Bumping package version to $1-$2"
+
+Version=$1
+SEMVER=${Version:1}
+
+if [ -z $2 ]
+then
+	sed -E "s/versionName .+/versionName \"$SEMVER\"/g" app/build.gradle > tempfile && cat tempfile > app/build.gradle && rm -f tempfile
+else
+	sed -E "s/versionName .+/versionName \"$SEMVER-$2\"/g" app/build.gradle > tempfile && cat tempfile > app/build.gradle && rm -f tempfile
+fi
+
+echo --------------------------
+echo "Done, Package now at $1-$2"
