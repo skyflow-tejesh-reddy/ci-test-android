b47158ca560a8f6da5938af263220e68962137ff
Tejesh <tejesh.allampati@zemosolabs.com>
2021-09-13 17:00:37 +0530

    public releaset

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -1,14 +1,21 @@
-echo "Bumping package version to $1-$2"
 
-Version=$1
-SEMVER=${Version:1}
+	Version=$1
+	SEMVER=${Version:1}
 
-if [ -z $2 ]
-then
-	sed -E "s/versionName .+/versionName \"$SEMVER\"/g" app/build.gradle > tempfile && cat tempfile > app/build.gradle && rm -f tempfile
-else
-	sed -E "s/versionName .+/versionName \"$SEMVER-dev$2\"/g" app/build.gradle > tempfile && cat tempfile > app/build.gradle && rm -f tempfile
-fi
+	if [ -z $2 ]
+	then
+		echo "Bumping package version to $1"
 
-echo --------------------------
-echo "Done, Package now at $1-$2"
+		sed -E "s/versionName .+/versionName \"$SEMVER\"/g" app/build.gradle > tempfile && cat tempfile > app/build.gradle && rm -f tempfile
+
+		echo --------------------------
+		echo "Done, Package now at $1"
+	else
+		echo "Bumping package version to $1-$2"
+
+		sed -E "s/versionName .+/versionName \"$SEMVER-dev.$2\"/g" app/build.gradle > tempfile && cat tempfile > app/build.gradle && rm -f tempfile
+		echo --------------------------
+
+		echo "Done, Package now at $1-dev.$2"
+	fi
+
